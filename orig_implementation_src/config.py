7c73b3887b88dba5e8b2d5c239749f70672301b2
Jonathan Levitsky <jlevitsky05@gmail.com>
2025-12-02 23:40:05 -0800

    feat(orig-u-net): original implementation in separate folder

---
+++ orig_implementation_src/config.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import torch
+
+# paths
+BASE_DIR = Path(__file__).resolve().parent
+PROJECT_ROOT = BASE_DIR.parent
+IMAGE_ROOT = PROJECT_ROOT / "PhC-C2DH-U373"
+PLOT_DIR = PROJECT_ROOT / "orig_implementation_src/orig_impl_plots"
+MODEL_SAVE_PATH = PROJECT_ROOT / "orig_impl_checkpoints"
+WEIGHT_MAP_CACHE_DIR = PROJECT_ROOT / "orig_impl_weight_cache"
+
+# run save tag
+RUN_DETAIL = "original_implementation"
+
+# data loading
+NUM_WORKERS = 12
+BATCH_SIZE = 1  # batch size detailed in the paper
+CROP_SIZE = 572
+
+# augmentation
+FLIP_PROBABILITY = 0.5
+DEFORM_SIGMA = 10.0
+DEFORM_POINTS = 3
+
+# weight map
+WEIGHT_MAP_W0 = 10.0
+WEIGHT_MAP_SIGMA = 5.0
+WEIGHT_MAP_CACHE_EXTENSION = ".npy"
+
+# model/training (detailed in paper)
+NUM_OUTPUT_CHANNELS = 2
+LEARNING_RATE = 0.01
+MOMENTUM_TERM = 0.99
+EPOCHS = 100
+
+# cuda configs
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+PIN_MEMORY = DEVICE.type == "cuda"
